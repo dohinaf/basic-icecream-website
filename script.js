a351273656f3584d1d996a5e116fcdc4606c920f
Dohina Felix Sunder <135969309+dohinafs@users.noreply.github.com>
2024-07-05 17:00:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let navbar = document.querySelector('.navbar');
+    let searchForm = document.querySelector('.search-form');
+    let cartItem = document.querySelector('.cart-items-container');
+
+    document.querySelector('#search-btn').onclick = () => {
+        searchForm.classList.toggle('active');
+        navbar.classList.remove('active');
+        cartItem.classList.remove('active');
+    };
+
+    document.querySelector('#cart-btn').onclick = () => {
+        cartItem.classList.toggle('active');
+        navbar.classList.remove('active');
+        searchForm.classList.remove('active');
+    };
+
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+        searchForm.classList.remove('active');
+        cartItem.classList.remove('active');
+    };
+});
+											
