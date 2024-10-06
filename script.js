fb73c08857f91948135f350c59a3787fd88bbe37
Rajatsinghal02 <singhalrajat354@gmail.com>
2024-10-06 19:50:00 +0530

    Mobile Responsive Navbar Created

--- script.js
+++ script.js
@@ -467,3 +467,11 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 });
+
+
+const menuBtn = document.querySelector('#menu-btn');
+const navbar = document.querySelector('.navbar');
+
+menuBtn.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
