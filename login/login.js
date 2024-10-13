8a39c231b8259a16fe538ae3f9e7d9846ae93418
Akanksha Pal <akankshapal1055@gmail.com>
2024-10-13 12:08:08 +0530

    login page done

--- login/login.js
+++ login/login.js
@@ -84,3 +84,22 @@ document.querySelector(".login-form").addEventListener("submit", function (e) {
   // Clear the form fields
   document.querySelector(".login-form").reset();
 });
+
+
+
+
+// password toggle feature
+function togglePasswordVisibility() {
+  const passwordInput = document.getElementById("password");
+  const eyeIcon = document.getElementById("eye-icon");
+  
+  if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      eyeIcon.classList.remove("fa-eye");
+      eyeIcon.classList.add("fa-eye-slash");
+  } else {
+      passwordInput.type = "password";
+      eyeIcon.classList.remove("fa-eye-slash");
+      eyeIcon.classList.add("fa-eye");
+  }
+}
